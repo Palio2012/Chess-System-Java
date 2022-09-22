7c150c654da16983b5a0635a5b2e497c299d7598
Palio2012 <daniasrayan.com@gmail.com>
2022-09-22 14:27:27 -0300

    CheckMate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,7 @@ public class Program {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
                 System.out.println();
+
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
