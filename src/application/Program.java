20a566e336bf5717586e89f617cdba2601480a47
Palio2012 <daniasrayan.com@gmail.com>
2022-09-22 11:40:03 -0300

    Implementing possible moves of king

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,21 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Program {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
-        while(true) {
+        while (true) {
             try {
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces());
@@ -25,17 +23,16 @@ public class Program {
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
-                boolean [] [] possibleMoves = chessMatch.possibleMoves(source);
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             }
-            catch(ChessException e) {
+            catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
